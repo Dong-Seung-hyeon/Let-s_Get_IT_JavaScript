1ac876096bf7f2bf0971e6128385c7212791f898
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-10 14:26:00 +0900

    Chapter 2.1.1 세미콜론

---
+++ js_basic.js
@@ -0,0 +1,6 @@
+// 2.1.1 세미콜론
+console('Hello, World!');
+console('Hello, World!');
+console('Hello, javascript!');
+console('Hello!');
+/* 세미콜론은 붙이지 않으면 일부 명령에서 가끔 에러가 발생, 혹시 모를 에러 발생을 방지하고, 통일성과 가독성을 위해 모든 명령 뒤에 세미콜론을 붙여준다. */
